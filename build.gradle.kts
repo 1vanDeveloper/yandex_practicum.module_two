ded875f407ed6c1cec533ba68789314fa70aadd0
Ivan Shpyakin <i.shpyakin@tinkoff.ru>
2026-03-21 14:27:38 +0300

    Добавил шаблоны и базовые зависимости

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,13 @@
 plugins {
     id("java")
+    id("org.springframework.boot") version "4.0.4"
+    id("io.spring.dependency-management") version "1.1.7"
+}
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(25)
+    }
 }
 
 group = "ru.yandex.practicum"
@@ -10,8 +18,25 @@ repositories {
 }
 
 dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation ("org.springframework.boot:spring-boot-starter-validation")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.projectlombok:lombok")
+    implementation("org.hibernate.validator:hibernate-validator")
+    implementation("jakarta.validation:jakarta.validation-api")
+    implementation("org.glassfish:jakarta.el:4.0.2")
+    implementation("org.apache.commons:commons-lang3:3.14.0")
+    implementation("com.fasterxml.jackson.core:jackson-databind")
+    implementation("com.fasterxml.jackson.core:jackson-core")
+    implementation("com.fasterxml.jackson.core:jackson-annotations")
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.6.0")
+    runtimeOnly("org.postgresql:postgresql")
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.springframework.boot:spring-boot-starter-webmvc-test")
+    testImplementation("org.springframework.boot:spring-boot-starter-actuator-test")
+    testImplementation("org.springframework.boot:spring-boot-starter-validation-test")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
