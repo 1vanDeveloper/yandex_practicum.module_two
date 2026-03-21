553ab616351809b71b8a7d1957d68c3b891dcd58
Ivan Shpyakin <i.shpyakin@tinkoff.ru>
2026-03-22 02:01:31 +0300

    Обновил методы контроллера до асинхронных

--- src/main/java/ru/yandex/practicum/controller/WebController.java
+++ src/main/java/ru/yandex/practicum/controller/WebController.java
@@ -1,5 +1,6 @@
 package ru.yandex.practicum.controller;
 
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,75 +9,93 @@ import org.springframework.web.bind.annotation.RequestParam;
 import ru.yandex.practicum.dto.*;
 
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
 
 @Controller
 class WebController {
 
+    @Async
     @GetMapping({"/", "/items"})
-    public String getItems(
+    public CompletableFuture<String> getItems(
             Model model,
             @RequestParam(required = false) String search,
             @RequestParam(required = false, defaultValue = "ALPHA") SortDto sort,
             @RequestParam(required = false, defaultValue = "10") Integer pageSize,
             @RequestParam(required = false, defaultValue = "1") Integer pageNumber) {
 
-        var viewData = new GetItemsViewDto(
-                search,
-                sort,
-                List.of(emptyItem()),
-                new PagingDto(
-                        pageNumber,
-                        pageSize,
-                        false,
-                        true));
+        return CompletableFuture.supplyAsync(() -> {
+            var viewData = new GetItemsViewDto(
+                    search,
+                    sort,
+                    List.of(emptyItem()),
+                    new PagingDto(
+                            pageNumber,
+                            pageSize,
+                            false,
+                            true));
 
-        model.addAttribute("search", viewData.search());
-        model.addAttribute("sort", viewData.sort().name());
-        model.addAttribute("items", viewData.items());
-        model.addAttribute("paging", viewData.paging());
-        return "items";
+            model.addAttribute("search", viewData.search());
+            model.addAttribute("sort", viewData.sort().name());
+            model.addAttribute("items", viewData.items());
+            model.addAttribute("paging", viewData.paging());
+            return "items";
+        });
+
     }
 
+    @Async
     @GetMapping("/items/{id}")
-    public String getItem(
+    public CompletableFuture<String>  getItem(
             Model model,
             @PathVariable long id) {
 
-        var viewData = emptyItem();
+        return CompletableFuture.supplyAsync(() -> {
+            var viewData = emptyItem();
 
-        model.addAttribute("item", viewData);
-        return "item";
+            model.addAttribute("item", viewData);
+            return "item";
+        });
     }
 
+    @Async
     @GetMapping("/cart/items")
-    public String getCart(Model model) {
-        var viewData = new GetCartViewDto(List.of(emptyItem()));
+    public CompletableFuture<String>  getCart(Model model) {
+        return CompletableFuture.supplyAsync(() -> {
+            var viewData = new GetCartViewDto(List.of(emptyItem()));
 
-        model.addAttribute("items", viewData.items());
-        model.addAttribute("total", viewData.total());
-        return "cart";
+            model.addAttribute("items", viewData.items());
+            model.addAttribute("total", viewData.total());
+            return "cart";
+        });
     }
 
+    @Async
     @GetMapping("/orders")
-    public String getOrders(Model model) {
-        var viewData = new GetOrdersViewDto(
-                List.of(
-                        new OrderDto(1, List.of(new OrderItemDto("title", 5, 50)))
-                )
-        );
+    public CompletableFuture<String>  getOrders(Model model) {
+        return CompletableFuture.supplyAsync(() -> {
+            var viewData = new GetOrdersViewDto(
+                    List.of(
+                            new OrderDto(1, List.of(new OrderItemDto("title", 5, 50)))
+                    )
+            );
 
-        model.addAttribute("orders", viewData.orders());
-        return "orders";
+            model.addAttribute("orders", viewData.orders());
+            return "orders";
+        });
     }
 
+    @Async
     @GetMapping("/orders/{id}")
-    public String getOrders(Model model, @PathVariable long id, @RequestParam(required = false) Boolean newOrder) {
+    public CompletableFuture<String>  getOrders(Model model,
+                                                @PathVariable long id,
+                                                @RequestParam(required = false) Boolean newOrder) {
+        return CompletableFuture.supplyAsync(() -> {
+            var title = newOrder != null && newOrder ? "new order" : "title";
+            var viewData = new OrderDto(id, List.of(new OrderItemDto(title, 5, 50)));
 
-        var title = newOrder != null && newOrder ? "new order" : "title";
-        var viewData = new OrderDto(id, List.of(new OrderItemDto(title, 5, 50)));
-
-        model.addAttribute("order", viewData);
-        return "order";
+            model.addAttribute("order", viewData);
+            return "order";
+        });
     }
 
     private static ItemDto emptyItem() {
