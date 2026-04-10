190cbab9267b0b43e1101216712b47f28f9ba802
Ivan Shpyakin <i.shpyakin@tinkoff.ru>
2026-04-10 20:14:31 +0300

    remove unused imports

--- src/main/java/ru/yandex/practicum/repository/UserRepository.java
+++ src/main/java/ru/yandex/practicum/repository/UserRepository.java
@@ -1,8 +1,6 @@
 package ru.yandex.practicum.repository;
 
-import org.springframework.data.r2dbc.repository.Query;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
-import org.springframework.data.repository.query.Param;
 import reactor.core.publisher.Mono;
 import ru.yandex.practicum.model.User;
 
