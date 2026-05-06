7f10370cef4d89f1c03f5b18c8e9794ed5c4fc15
Ivan Shpyakin <i.shpyakin@tinkoff.ru>
2026-05-06 23:08:00 +0300

    remove except maven repositories

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,10 +4,6 @@ plugins {
 
 allprojects {
     repositories {
-        maven {
-            url = uri("https://artifactory.tcsbank.ru/artifactory/maven-all")
-            name = "maven-all"
-        }
         mavenCentral()
     }
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,13 +1,7 @@
-import org.gradle.kotlin.dsl.maven
-
 rootProject.name = "market"
 
 pluginManagement {
     repositories {
-        maven {
-            url = uri("https://artifactory.tcsbank.ru/artifactory/maven-all")
-            name = "maven-all"
-        }
         mavenCentral()
         gradlePluginPortal()
     }
